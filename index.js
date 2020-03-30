8f643eddb118a5006dfa5346d78ea631d33ca6d7
Mae Kangsadan <maemeier@live.com>
2020-03-30 22:54:51 +0200

    change shadow

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fyniShadow(options) {
   let images = document.querySelectorAll("./fyniShadow");
 
-  if (options.shadow_type === "beauty") options.shadow_type = "0px";
-  else options.shadow_type = "15px";
+  if (options.shadow_type === "beauty")
+    options.shadow_type = "box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);";
+  else options.shadow_type === "float";
+  options.shadow_type = "box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.15)";
 
   images.forEach(image => {
-    image.style.boxShadow = `10px 10px ${options.shadow_type} 0px 15px rgba(0,0,0,0.15)`;
+    image.style.boxShadow = `10px 10px ${options.shadow_type} 0px 15px 20px rgba(0, 0, 0, 0.15)`;
 
     if (options.padding) {
       image.style.padding = "1em";
